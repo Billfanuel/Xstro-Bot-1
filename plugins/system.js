ecbc27bc2f6f7e91557391ecd9d1cbe551675ce6
Astro <astrofx0011@gmail.com>
2024-07-03 19:40:05 +0100

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,11 +1,19 @@
-const { Index } = require("../lib");
-const { menu } = require("../lib/Base/class");
+const { Index, ping } = require('../lib')
+const { menu } = require('../lib/Base/class')
 
-Index({
-    pattern: 'menu',
-    desc: 'Get All Commands',
-    category: 'system'
-,
-},
-menu
+Index(
+ {
+  pattern: 'menu',
+  desc: 'Get All Commands',
+  category: 'system',
+ },
+ menu
+)
+Index(
+ {
+  pattern: 'ping',
+  desc: 'Ping Sever',
+  category: 'system',
+ },
+ ping
 )
